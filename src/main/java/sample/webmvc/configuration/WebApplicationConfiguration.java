652587db32e33a2f752f03dcbbc8e1385e004027
Jasvant9149 <jasvantmaurya61@gmail.com>
2026-09-16 17:14:13 +0530

    update

---
+++ src/main/java/sample/webmvc/configuration/WebApplicationConfiguration.java
@@ -0,0 +1,30 @@
+package sample.webmvc.configuration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRegistration;
+
+public class WebApplicationConfiguration implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		
+		
+		AnnotationConfigWebApplicationContext annWebConfig =  new AnnotationConfigWebApplicationContext();
+	
+	annWebConfig.register(SpringConfiguration.class);
+	
+	annWebConfig.setServletContext(servletContext);
+	ServletRegistration.Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(annWebConfig));
+	servlet.setLoadOnStartup(1);
+	servlet.addMapping("/");
+	
+		
+	}
+
+}
