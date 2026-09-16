b37acd7622177e2a8c183c608e039999d179a8d5
Jasvant9149 <jasvantmaurya61@gmail.com>
2026-09-16 15:36:31 +0530

    Initial commit

---
+++ src/main/java/sample/webmvc/controller/UserController2.java
@@ -0,0 +1,31 @@
+package sample.webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/kitchen")
+public class UserController2 {
+	
+	@RequestMapping("/")
+
+	public String greet() {
+		System.out.println("UsrController.welcome");
+		return "kitchen";
+	}
+	
+	
+	// /WEB-INF/JSP/welcome.jsp
+	
+	//@RequestMapping(value="/movies" , method = RequestMethod.GET)
+	@GetMapping(value = {"/movies", "/films" })
+	
+	public String movie() {
+		System.out.println("UsrController.movie");
+		return "movies";
+	}
+
+}
---
+++ src/main/java/sample/webmvc/controller/UserController3.java
@@ -0,0 +1,31 @@
+package sample.webmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/ele")
+public class UserController3 {
+	
+	@RequestMapping("/")
+
+	public String greet() {
+		System.out.println("UsrController.welcome");
+		return "welcome";
+	}
+	
+	
+	// /WEB-INF/JSP/welcome.jsp
+	
+	//@RequestMapping(value="/movies" , method = RequestMethod.GET)
+	@GetMapping(value = {"/movies", "/films" })
+	
+	public String movie() {
+		System.out.println("UsrController.movie");
+		return "movies";
+	}
+
+}
